7ac7e981fc06e9bd26f781f031cf61141d99b939
leesg1 <leesg@tcd.ie>
2017-08-21 13:56:20 +0000

    up to user auth

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground"),
+    seedDB = require("./seeds"),
+    Comment = require("./models/comment")
+//    User = require("./models/user");
 
 
- var campgrounds = [
-        {name: "Salmon Creek", image:"https://www.w3schools.com/css/img_lights.jpg"},
-        {name: "Cod Creek", image:"https://www.w3schools.com/css/img_fjords.jpg"},
-        {name: "Tuna Creek", image:"https://camo.mybb.com/e01de90be6012adc1b1701dba899491a9348ae79/687474703a2f2f7777772e6a71756572797363726970742e6e65742f696d616765732f53696d706c6573742d526573706f6e736976652d6a51756572792d496d6167652d4c69676874626f782d506c7567696e2d73696d706c652d6c69676874626f782e6a7067"},
-        {name: "Salmon Creek", image:"https://www.w3schools.com/css/img_lights.jpg"},
-        {name: "Cod Creek", image:"https://www.w3schools.com/css/img_fjords.jpg"},
-        {name: "Salmon Creek", image:"https://www.w3schools.com/css/img_lights.jpg"},
-        {name: "Cod Creek", image:"https://www.w3schools.com/css/img_fjords.jpg"},
-        {name: "Salmon Creek", image:"https://www.w3schools.com/css/img_lights.jpg"},
-        {name: "Cod Creek", image:"https://www.w3schools.com/css/img_fjords.jpg"}
-        ];
-        
-        
-        
+    
+
+mongoose.connect("mongodb://localhost/yelp_camp")
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname +"/public"))
+seedDB();    
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Website is running!");
@@ -28,24 +25,92 @@ app.get("/", function(req,res){
     res.render("landing");
 })
 
+
+// INDEX - show all campgrounds
 app.get("/campgrounds", function(req,res){
-    res.render("campgrounds",{campgrounds:campgrounds});
+    // get all campgrounds from db
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else
+        {
+            res.render("campgrounds/index",{campgrounds:allCampgrounds});
+        }
+    })
 })
 
+// CREATE - add new campground to database
 app.post("/campgrounds",function(req, res){
     // get data from for and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
+    var desc = req.body.description;
+
     
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    
-    res.redirect("/campgrounds");
-    //redirect back to the campgriunds page
-    
+    var newCampground = {name: name, image: image, description: desc};
+    // Create a new campground and save to the database
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err)
+        } else{
+            res.redirect("/campgrounds");
+        }
+    })
+    //redirect back to the campgrounds page
 });
 
+// NEW - show form to create a new campground
 app.get("/campgrounds/new", function(req,res){
-    res.render("new.ejs");
+    res.render("campgrounds/new");
     
 })
+
+// Comment routes
+app.get("/campgrounds/:id/comments/new", function(req, res){
+        Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground : campground});
+        }
+    })
+})
+
+app.post("/campgrounds/:id/comments", function(req,res){
+    // lookup campground using id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds")
+        } else{
+            console.log(req.body.comment)
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err)
+                } else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id)
+                }
+            })
+        }
+    })
+    
+    //create new comments
+    //connect new comment to campground
+    //redirect to campground show page
+    
+})
+
+//SHOW - show info about specific campground
+app.get("/campgrounds/:id", function(req,res){
+    // find the campground with a certain id
+    
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err)
+        } else{
+            res.render("campgrounds/show", {campground : foundCampground})
+        }
+    });
+});
---
+++ seeds.js
@@ -0,0 +1,59 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+var data = [
+        {name: "Salmon Creek", 
+        image:"https://www.w3schools.com/css/img_lights.jpg", 
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim veniam sint, eaque voluptatem possimus facilis, corporis quas quia molestias iusto reprehenderit nulla fugit dolorum quasi totam asperiores blanditiis minima natus."},
+        
+        {name: "Cod Creek", 
+        image:"https://www.w3schools.com/css/img_fjords.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim veniam sint, eaque voluptatem possimus facilis, corporis quas quia molestias iusto reprehenderit nulla fugit dolorum quasi totam asperiores blanditiis minima natus."
+        },
+        
+        {name: "Tuna Creek", 
+        image:"https://camo.mybb.com/e01de90be6012adc1b1701dba899491a9348ae79/687474703a2f2f7777772e6a71756572797363726970742e6e65742f696d616765732f53696d706c6573742d526573706f6e736976652d6a51756572792d496d6167652d4c69676874626f782d506c7567696e2d73696d706c652d6c69676874626f782e6a7067",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim veniam sint, eaque voluptatem possimus facilis, corporis quas quia molestias iusto reprehenderit nulla fugit dolorum quasi totam asperiores blanditiis minima natus."}
+ 
+    ]
+
+function seedDB(){
+    // remove all campgrounds
+    Campground.remove({},function(err){
+        if(err){
+            console.log(err)
+        } else{
+            console.log("Removed Campgrounds");
+        }
+        
+        // add some campgrounds
+        data.forEach(function(seed){
+        Campground.create(seed, function(err,data){
+            if(err){
+                console.log(err);
+            } else{
+                console.log("added a campground");
+                 // add some comments
+                Comment.create(
+                    {
+                        text: "Love this place",
+                        author: "Homer"
+                    },
+                    function(err, comment){
+                        if(err){
+                            console.log(err);
+                        }
+                        data.comments.push(comment);
+                        data.save();
+                        console.log("Created new comment");
+                        
+                    });
+            }
+        })
+    })
+    })
+    
+   
+}
+
+module.exports = seedDB
